547460785bacb86a08981b884bc4d2e5f91232b2
Ferly Afriliyan - Ryougaa <122451961+ferlyafriliyan@users.noreply.github.com>
2023-11-23 05:45:57 +0700

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import setup
 
 if __name__ == '__main__':
 	try:
-		example.clear_terminal()
-		example.get_input_file()
+		setup.clear_terminal()
+		setup.get_input_file()
 	except (KeyboardInterrupt, Exception) as e:
 		exit(f"[Error] {str(e).capitalize()}!")
