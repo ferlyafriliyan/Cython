380c93aa62b093d2854c91027beadf65b02ac359
Ferly Afriliyan - Ryougaa <122451961+ferlyafriliyan@users.noreply.github.com>
2023-11-23 05:40:45 +0700

    Add files via upload

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
-import self
+import setup
 
 if __name__ == '__main__':
-	self.main()
+	try:
+		example.clear_terminal()
+		example.get_input_file()
+	except (KeyboardInterrupt, Exception) as e:
+		exit(f"[Error] {str(e).capitalize()}!")
